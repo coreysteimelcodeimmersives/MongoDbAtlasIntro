3e12276a819c3a8dc3f8743983144212f0f2734c
Corey Steimel <coreysteimel@Coreys-MacBook-Pro-Work.local>
2022-05-24 11:35:14 -0600

    all script commands working properly

---
+++ mongoAtlas.js
@@ -0,0 +1,72 @@
+// *** Create a new collection blogPosts ***
+// const blogPosts = [
+//     {
+//       createdAt: "2022-03-22T10:36:37.176Z",
+//       title: "dicta",
+//       text: "Iusto et in et. Nulla accusantium fugit. Et qui dolorem inventore soluta et veritatis. Aut ut aut non laudantium eveniet suscipit odit. Sapiente sint nihil nihil sit et molestias. In nisi omnis quas et sed aut minus aperiam ea.\n \rLaudantium quo quisquam quae. Et et quas officia perspiciatis iusto sunt sunt eaque. Quidem sit voluptas deserunt sequi magni.\n \rEst est facere cumque ipsam omnis animi. Voluptatem magnam officiis architecto possimus. Quia similique aut eos qui. Quasi quae sed aliquam.",
+//       author: "Darren Abbott",
+//       id: "1",
+//     },
+//     {
+//       createdAt: "2022-03-22T15:16:56.285Z",
+//       title: "ducimus",
+//       text: "Placeat ea et fuga. Qui itaque quibusdam nam. Maxime nobis quam. Et laudantium sunt incidunt reiciendis.\n \rEarum aut sed omnis autem aliquam architecto corporis sint. Nostrum cumque voluptatem aperiam alias similique. Tenetur et esse omnis praesentium ipsum alias. Impedit rerum qui quia quaerat architecto mollitia est autem. Qui blanditiis earum et qui dolorum reprehenderit. Debitis est temporibus.\n \rEt nam sed. Corporis ut rerum. Ut qui dolore est dolorem ex.",
+//       author: "Luke Rogahn PhD",
+//       id: "2",
+//     },
+//     {
+//       createdAt: "2022-03-21T20:09:32.298Z",
+//       title: "quod",
+//       text: "Accusamus nisi eos. Tenetur earum tenetur nemo. Qui voluptas temporibus repellendus maxime. Ipsum optio voluptate enim nihil. Ea et dolorem. Omnis unde perspiciatis.\n \rUt odio eaque. Harum non placeat. Eveniet molestiae in cupiditate dolor doloremque rerum eligendi aut ab.\n \rMolestias eligendi et. Nemo velit natus autem numquam atque provident et nulla. In et dolores ad nihil. Delectus quis doloremque asperiores similique. Asperiores id nam vitae nobis labore autem. Dolor aperiam provident quia consectetur aut ut.",
+//       author: "Maryann Schneider",
+//       id: "3",
+//     },
+//     {
+//       createdAt: "2022-03-21T23:07:53.447Z",
+//       title: "ut",
+//       text: "Itaque necessitatibus repudiandae. Porro suscipit exercitationem qui atque. Perferendis suscipit debitis sint aut dignissimos nobis ut. Modi ea nihil est vel consequuntur voluptatem. In magnam delectus in eos reiciendis sit est enim eligendi. Sint dicta at.\n \rConsectetur aspernatur alias sed non explicabo blanditiis laborum fugit voluptate. Reiciendis iste aut sit natus qui et in ratione. Placeat qui in voluptatum autem nulla ratione. Commodi sit alias sint sapiente rem. Quia sapiente minus deleniti vitae.\n \rExercitationem numquam omnis maxime dolorum sed deserunt suscipit laudantium. Ad et autem voluptatem esse laudantium et. Id fuga accusamus est sapiente dicta.",
+//       author: "Dr. Lorenzo Anderson",
+//       id: "4",
+//     },
+//     {
+//       createdAt: "2022-03-22T15:14:39.819Z",
+//       title: "id",
+//       text: "Porro officia aliquid fugiat sed reprehenderit illo amet doloribus sed. Molestiae vero et. Quae voluptates dolores. Voluptatem facere fuga. Veniam perferendis illo ut sunt earum deleniti.\n \rIusto neque dolorem esse error. Saepe et quia ut corrupti. Autem repellendus similique dolorem sunt in ipsa perferendis. Et excepturi ut voluptatem deserunt accusantium dolores aperiam cum ut.\n \rDoloremque expedita sit et voluptatem unde libero. Numquam beatae sed repellat iusto doloribus fugit tenetur. Possimus et ut adipisci harum voluptatem provident consequatur. Corporis quo aut vel itaque blanditiis illum.",
+//       author: "Bobbie Dach",
+//       id: "5",
+//     },
+//   ];
+//   db.posts.insertMany(blogPosts);
+
+// *** Insert a new blog post ***
+// let newBlog = {
+//     createdAt: new Date().toISOString(),
+//     title: 'A Review of Roman History',
+//     text: 'Back in the day, there was a guy named Julius Ceasar. And he was an awewsome general that kicked major butt. In fact he was so awesome that all his soldiers loved him and pledged loyalty to him. With the help of his men, he took power of Rome.',
+//     author: 'Corey S',
+//     id: String(db.posts.count() + 1)
+// }
+// db.posts.insertOne(newBlog);
+
+// *** Read the new blog post ***
+// db.posts.findOne({id: String(db.posts.count())})
+
+// *** Update the new blog post with a new title ***
+// db.posts.updateOne(
+//     {id: String(db.posts.count())}, 
+//     {$set: {title: 'Brief Overview of Roman History v2'}})
+
+// *** Delete the new blog post ***
+// db.posts.deleteOne(
+//     {id: String(db.posts.count())}
+// )
+
+
+
+
+// db.posts.find({}).limit(100)
+
+
+
+
+
